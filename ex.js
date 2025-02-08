dbd6f3b2e84c034b885e331c65816dc6a7a76ac8
Seckene Sako <seckene_sako@hotmail.com>
2025-02-08 15:53:26 +0100

    huit

--- ex.js
+++ ex.js
@@ -1,14 +1,29 @@
 const tete =document.querySelector("p");
+const svv =document.querySelector(".svv");
+const burger =document.querySelector(".burger");
+const closeee =document.querySelector(".close");
+console.log(closeee);
+console.log(burger);
+console.log(svv);
 console.log(tete);
 window.addEventListener("scroll",()=>{
-if (scrollY>100) {
+if (scrollY<10) {
     tete.style.visibility="hidden";
 
 }
 else{
-    scrollY>90
+    scrollY>50;
     tete.style.visibility="visible";
     
 }
+});
+
+svv.addEventListener("click",()=>{
+    burger.style.display="block";
+     closeee.style.display="block"
 })
 
+closeee.addEventListener("click",()=>{
+    burger.style.display="none";
+    closeee.style.display="none"
+})
