032a01e7f71b07acfea1e2e018a1b71936eae1b3
Seckene Sako <seckene_sako@hotmail.com>
2025-02-10 23:00:12 +0100

    huitv11

--- ex.js
+++ ex.js
@@ -2,10 +2,12 @@ const tete =document.querySelector("p");
 const svv =document.querySelector(".svv");
 const burger =document.querySelector(".burger");
 const closeee =document.querySelector(".close");
+const btn1 =document.querySelector("#btn1");
 console.log(closeee);
 console.log(burger);
 console.log(svv);
 console.log(tete);
+console.log(btn1)
 window.addEventListener("scroll",()=>{
 if (scrollY<10) {
     tete.style.visibility="hidden";
@@ -26,4 +28,11 @@ svv.addEventListener("click",()=>{
 closeee.addEventListener("click",()=>{
     burger.style.display="none";
     closeee.style.display="none"
+})
+
+
+
+btn1.addEventListener("click",()=>{
+   btn1.className ="lolo";
+   
 })
