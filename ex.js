0e5963761ef744a7858bffae968f29f7d2d42a75
Seckene Sako <seckene_sako@hotmail.com>
2025-04-04 20:34:23 +0200

    first7

---
+++ ex.js
@@ -0,0 +1,12 @@
+let reste =document.querySelector(".reste")
+let burger=document.querySelector(".boss")
+console.log(reste)
+burger.addEventListener("click",()=>{
+    reste.style.display= "block" ;
+    burger.style.display="none"
+})
+
+
+
+
+
