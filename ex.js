1ee46bb3aa3eb912bd32dcf51274606f142a5df0
Seckene Sako <seckene_sako@hotmail.com>
2025-04-06 22:10:41 +0200

    first29

--- ex.js
+++ ex.js
@@ -16,3 +16,10 @@ croix.addEventListener("click",()=>{
   burger.style.display="flex"
     reste.style.display= "none" ;   
 })
+
+
+let passtext = document.querySelector(".passtext");
+
+passtext.addEventListener("mouseover", (e) => {
+    passtext.style.visibility = "visible";
+});
